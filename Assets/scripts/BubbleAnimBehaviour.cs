6819ce4d2eb8db85a6e0ea9276bd7f40e52bf782
KuroMicho <kevin0552009.kr@gmail.com>
2022-07-03 16:00:18 -0500

    Assets

---
+++ Assets/scripts/BubbleAnimBehaviour.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class BubbleAnimBehaviour : MonoBehaviour
+{
+    private RectTransform BubbleRect;
+    private TMP_Text BubbleText;
+
+    private void Start()
+    {
+        BubbleRect = GetComponent<RectTransform>();
+        BubbleText = GetComponent<TMP_Text>();
+    }
+
+    public void Up()
+    {
+        BubbleText.DOFade(255, 0.5f);
+        BubbleRect.DOAnchorPosY(20, 0.5f).SetEase(Ease.InBack);
+        Down();
+    }
+
+    public void Down()
+    {
+        BubbleText.DOFade(0, 0.5f).SetDelay(0.5f);
+        BubbleRect.DOAnchorPosY(0, 0.5f).SetEase(Ease.OutBack).SetDelay(0.5f);
+    }
+}
